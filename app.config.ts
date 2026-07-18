252707d38eaca562956ea842c7a0c6c680f70fe4
petenoempire <129602128+petenoempire@users.noreply.github.com>
2026-07-18 00:08:34 -0700

    Create app.config.ts

---
+++ app.config.ts
@@ -0,0 +1,26 @@
+import { defineConfig } from "vinxi";
+import { tanstackStart } from "@tanstack/react-start/plugin";
+
+export default defineConfig({
+  routers: [
+    {
+      name: "public",
+      type: "static",
+      dir: "./public",
+      base: "/",
+    },
+    {
+      name: "client",
+      type: "spa",
+      handler: "./src/entry-client.tsx",
+      target: "browser",
+      plugins: () => [tanstackStart()],
+    },
+    {
+      name: "ssr",
+      type: "http",
+      handler: "./src/entry-server.tsx",
+      target: "server",
+    }
+  ]
+});
