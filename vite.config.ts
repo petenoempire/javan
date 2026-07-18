6231d9449bf5ae97b72a882f7315f08ec491ac32
petenoempire <129602128+petenoempire@users.noreply.github.com>
2026-07-17 20:31:52 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  plugins: [TanStackRouterVite(), react(), tsconfigPaths()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3001',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
-    },
-  },
+  plugins: [react(), tsconfigPaths()],
 });
